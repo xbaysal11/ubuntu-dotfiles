480df24258ec7075444235a2a2f49935ca36d632
xbaysal11 <baisalbek.daniiarov@gmail.com>
2019-05-20 23:27:28 +0600

    :seedling: ADDED font awesome

--- polybar/scripts/upgradecheck.sh
+++ polybar/scripts/upgradecheck.sh
@@ -3,7 +3,7 @@ echo >  ~/.config/polybar/scripts/upgradecheck
 /usr/lib/update-notifier/apt-check --human-readable >  ~/.config/polybar/scripts/upgradecheck 
 if grep -q "0 packages can be updated." ~/.config/polybar/scripts/upgradecheck  ;
 then
-  echo "" &
+  echo "" &
 else
-  echo "" &
+  echo "" &
 fi
