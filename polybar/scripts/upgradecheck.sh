8bbd65024f18afc0fac5c8e7cb16d8ae7eaf4c16
xbaysal11 <baisalbek.daniiarov@gmail.com>
2019-05-22 23:38:52 +0600

    :partly_sunny: MODIFIED updates checker script now shows number

--- polybar/scripts/upgradecheck.sh
+++ polybar/scripts/upgradecheck.sh
@@ -3,12 +3,12 @@ echo >  ~/.config/polybar/scripts/upgradecheck
 /usr/lib/update-notifier/apt-check --human-readable >  ~/.config/polybar/scripts/upgradecheck 
 if grep -q "0 packages can be updated." ~/.config/polybar/scripts/upgradecheck  ;
 then
-  num=`/usr/lib/update-notifier/apt-check 2>&1 | cut -d ';' -f 2`
+  num=`/usr/lib/update-notifier/apt-check 2>&1 | cut -d ';' -f 1`
   icon=""
   res="$icon: $num"
   echo $res
 else
-  num=`/usr/lib/update-notifier/apt-check 2>&1 | cut -d ';' -f 2`
+  num=`/usr/lib/update-notifier/apt-check 2>&1 | cut -d ';' -f 1`
   icon=""
   res="$icon: $num"
   echo $res
