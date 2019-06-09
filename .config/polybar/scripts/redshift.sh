4ea5eb1c00481ac2e0b695d5e02aecfb4d56248a
xbaysal11 <baisalbek.daniiarov@gmail.com>
2019-06-09 23:27:02 +0600

    :seedling: ADDED Disabling polybar

--- .config/polybar/scripts/redshift.sh
+++ .config/polybar/scripts/redshift.sh
@@ -2,12 +2,10 @@
 
 case "$1" in
     --night)
-        echo "234"
         redshift -O 3000
         ;;
     --day)
         redshift -x
-        echo "1111"
         ;;
     *)
         echo  ""
--- .config/polybar/scripts/vpn.sh
+++ .config/polybar/scripts/vpn.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-IP=$(curl -s https://ipvigilante.com/$(curl -s https://ipinfo.io/ip) | jq ' .data.ipv4, "",.data.country_name,"-",.data.subdivision_1_name,"-",.data.city_name' | tr -d '"') 
+IP=$(curl -s https://ipvigilante.com/$(curl -s https://ipinfo.io/ip) | jq ' .data.ipv4, "",.data.country_name,"-",.data.subdivision_1_name,"-", .data.city_name' | tr -d '"') 
 
 if pgrep -x openvpn > /dev/null; then
     echo  $IP 
