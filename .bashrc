74f9d7153b2aa8578fe9243fe4a2ca28b80f5662
xbaysal11 <baisalbek.daniiarov@gmail.com>
2020-01-08 12:12:08 +0600

    ADD monitors variables globolize

--- .bashrc
+++ .bashrc
@@ -117,3 +117,6 @@ if ! shopt -oq posix; then
 fi
 
 [ -f ~/.fzf.bash ] && source ~/.fzf.bash
+
+# Add RVM to PATH for scripting. Make sure this is the last PATH variable change.
+export PATH="$PATH:$HOME/.rvm/bin"
--- .config/polybar/scripts/mail.py
+++ .config/polybar/scripts/mail.py
@@ -1,11 +1,11 @@
 import imaplib
-gmail=''
-passwd=''
-obj = imaplib.IMAP4_SSL('imap.gmail.com',993)
-obj.login(gmail,passwd)
+gmail = 'baisalbek.daniiarov@gmail.com'
+passwd = 'daniyarovbaysalbek'
+obj = imaplib.IMAP4_SSL('imap.gmail.com', 993)
+obj.login(gmail, passwd)
 obj.select()
 number = len(obj.search(None, 'UnSeen')[1][0].split())
-if number>0:
+if number > 0:
     print(number)
 else:
     print('')
